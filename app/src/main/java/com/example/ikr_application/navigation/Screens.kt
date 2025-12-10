a2fa8bf7e4bf255a592eb4ad77f55f4ee265527c
spl3g <notspl3g@duck.com>
2025-12-10 15:38:25 +0300

    feat: add spl3g button

--- app/src/main/java/com/example/ikr_application/navigation/Screens.kt
+++ app/src/main/java/com/example/ikr_application/navigation/Screens.kt
@@ -3,14 +3,14 @@ package com.example.ikr_application.navigation
 import androidx.annotation.StringRes
 import androidx.fragment.app.Fragment
 import com.example.ikr_application.R
-import com.example.ikr_application.dimmension.ui.NamesFragment
 import com.example.ikr_application.nfirex.ui.MyFragment
-import com.example.ikr_application.spl3g.ui.MyFragment
+import com.example.ikr_application.spl3g.ui.Spl3gFragment
 
 enum class Screens(
     @StringRes val title: Int,
     val type: Class<out Fragment>,
 ) {
     SAMPLE(R.string.title_sample, MyFragment::class.java),
-    NAMES(R.string.title_dimmension_screen, NamesFragment::class.java)
+    NAMES(R.string.title_dimmension_screen, NamesFragment::class.java),
+	SPL3G(R.string.title_spl3g, Spl3gFragment::class.java),
 }
