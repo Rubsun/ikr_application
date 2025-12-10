8b56ea77a1cd7b9d74a227e922d2a4f5737471b7
KristevT <timkristev.cloud@gmail.com>
2025-12-10 17:30:20 +0300

    kristevt test

---
+++ app/src/main/java/com/example/ikr_application/kristevt/domain/ElapsedTimeUseCase.kt
@@ -0,0 +1,11 @@
+package com.example.ikr_application.kristevt.domain
+
+import com.example.ikr_application.kristevt.data.DeviceRepository
+
+class ElapsedTimeUseCase() {
+    fun value(precisions: TimePrecisions): Long {
+        val elapsedTime = DeviceRepository.INSTANCE.deviceInfo().elapsedTime
+
+        return elapsedTime / precisions.divider.inWholeMilliseconds
+    }
+}
