fd95099db14ee7891c9465f178683bd9f92c90da
ARTEM-kaa <artem.s.krivenko@yandex.ru>
2025-12-10 17:05:03 +0300

    ArtemkaaProject

--- app/src/main/java/com/example/ikr_application/navigation/Screens.kt
+++ app/src/main/java/com/example/ikr_application/navigation/Screens.kt
@@ -7,6 +7,7 @@ import com.example.ikr_application.akiko23.ui.Akiko23Fragment
 import com.example.ikr_application.dimmension.ui.NamesFragment
 import com.example.ikr_application.nfirex.ui.MyFragment
 import com.example.ikr_application.spl3g.ui.Spl3gFragment
+import com.example.ikr_application.artemkaa.ui.ArtemkaaFragment
 
 enum class Screens(
     @StringRes val title: Int,
@@ -21,4 +22,5 @@ enum class Screens(
     AKIKO23(R.string.title_akiko23, Akiko23Fragment::class.java),
 	SPL3G(R.string.title_spl3g, Spl3gFragment::class.java),
     DENISOVA(R.string.title_denisova, com.example.ikr_application.denisova.ui.DenisovaFragment::class.java),
+    ARTEMKAA(R.string.title_artemkaa, com.example.ikr_application.artemkaa.ui.ArtemkaaFragment::class.java),
 }
