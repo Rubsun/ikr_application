4c88f05713d2944c9c15c8c0ffbab829016e507c
roganofff <matematic1337@gmail.com>
2025-12-10 16:13:29 +0300

    Added Roganov(a.k.a. Quo Vadis) Screen

--- app/src/main/java/com/example/ikr_application/navigation/Screens.kt
+++ app/src/main/java/com/example/ikr_application/navigation/Screens.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import com.example.ikr_application.R
 import com.example.ikr_application.nfirex.ui.MyFragment
 import com.example.ikr_application.spl3g.ui.Spl3gFragment
+import com.example.ikr_application.quovadis.ui.CatFragment
 
 enum class Screens(
     @StringRes val title: Int,
@@ -13,4 +14,5 @@ enum class Screens(
     SAMPLE(R.string.title_sample, MyFragment::class.java),
     NAMES(R.string.title_dimmension_screen, NamesFragment::class.java),
 	SPL3G(R.string.title_spl3g, Spl3gFragment::class.java),
+    QUOVADIS(R.string.title_quovadis, CatFragment::class.java)
 }
