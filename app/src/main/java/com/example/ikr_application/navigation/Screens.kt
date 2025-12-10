5b4d397ecb5bab39eb119cfc69c66dad3e3e8583
Nn Zagora <nnnnzag625@gmail.com>
2025-12-10 18:07:27 +0300

    fix dog app

--- app/src/main/java/com/example/ikr_application/navigation/Screens.kt
+++ app/src/main/java/com/example/ikr_application/navigation/Screens.kt
@@ -16,6 +16,7 @@ import com.example.ikr_application.grigoran.ui.GrigoranFragment
 import com.example.ikr_application.quovadis.ui.CatFragment
 import com.example.ikr_application.rubsun.ui.NumberFragment
 import com.example.ikr_application.egorik4.ui.Egorik4Fragment
+import com.example.ikr_application.zagora.ui.FragmentDog
 
 enum class Screens(
     @StringRes val title: Int,
@@ -40,4 +41,5 @@ enum class Screens(
     DRAIN678(R.string.title_drain678, Drain678Fragment::class.java),
     MICHAELNOSKOV(R.string.title_michaelnoskov, com.example.ikr_application.michaelnoskov.ui.MyFragment::class.java),
     EGORIK4(R.string.title_egorik4, Egorik4Fragment::class.java),
+    ZAGORA(R.string.title_zagora, FragmentDog::class.java)
 }
