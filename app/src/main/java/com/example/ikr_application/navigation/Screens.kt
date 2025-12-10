8aa56e5393dee4e743d7f07af7df558d67b212c3
spl3g <notspl3g@duck.com>
2025-12-10 16:42:06 +0300

    Revert "Merge pull request #9 from Rubsun/roganov"
    This reverts commit 18e1c9a914531de97a075d195fd1fa6de269a03e, reversing
    changes made to 63551ac17ed74af22ebc9e3dac3a6f1e31b0bfde.

--- app/src/main/java/com/example/ikr_application/navigation/Screens.kt
+++ app/src/main/java/com/example/ikr_application/navigation/Screens.kt
@@ -7,9 +7,6 @@ import com.example.ikr_application.akiko23.ui.Akiko23Fragment
 import com.example.ikr_application.dimmension.ui.NamesFragment
 import com.example.ikr_application.nfirex.ui.MyFragment
 import com.example.ikr_application.spl3g.ui.Spl3gFragment
-import com.example.ikr_application.quovadis.ui.CatFragment
-import com.example.ikr_application.zagora.ui.FragmentDog
-import com.example.ikr_application.dimmension.ui.NamesFragment
 
 enum class Screens(
     @StringRes val title: Int,
@@ -23,8 +20,5 @@ enum class Screens(
     NAMES(R.string.title_dimmension_screen, NamesFragment::class.java),
     AKIKO23(R.string.title_akiko23, Akiko23Fragment::class.java),
 	SPL3G(R.string.title_spl3g, Spl3gFragment::class.java),
-    QUOVADIS(R.string.title_quovadis, CatFragment::class.java)
     DENISOVA(R.string.title_denisova, com.example.ikr_application.denisova.ui.DenisovaFragment::class.java),
-    GAF(R.string.dog, FragmentDog::class.java)
-
 }
--- app/src/main/java/com/example/ikr_application/quovadis/data/Cat.kt
+++
@@ -1,6 +0,0 @@
-package quo.vadis.sirius.data
-
-data class Cat(
-    val name: String,
-    val phrase: String?,
-)
