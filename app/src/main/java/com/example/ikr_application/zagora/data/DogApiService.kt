3fb7ef3f19596016b47a242d98ae90ab09a9d149
Nn Zagora <nnnnzag625@gmail.com>
2025-12-15 21:54:28 +0300

    impove dog viewer

--- app/src/main/java/com/example/ikr_application/zagora/data/DogApiService.kt
+++ app/src/main/java/com/example/ikr_application/zagora/data/DogApiService.kt
@@ -1,11 +1,20 @@
 package com.example.ikr_application.zagora.data
 
 import retrofit2.http.GET
+import retrofit2.http.Path
 
 interface DogApiService {
-    @GET("breeds/image/random")
-    suspend fun getRandomDogImage(): DogImageDto
+    @GET("breeds/list/all")
+    suspend fun getBreeds(): BreedListDto
+
+    @GET("breed/{breed}/images/random")
+    suspend fun getDogImageForBreed(@Path("breed") breed: String): DogImageDto
 }
+
+data class BreedListDto(
+    val message: Map<String, List<String>>,
+    val status: String
+)
 
 data class DogImageDto(
     val message: String,
