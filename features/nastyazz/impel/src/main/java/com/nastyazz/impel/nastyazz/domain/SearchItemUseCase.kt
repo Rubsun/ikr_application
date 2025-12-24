2f291a920cb4d4bb971471d8e5e9a880a82068e9
nastyazz <nastayz0808@mail.ru>
2025-12-24 15:36:50 +0300

    fix

--- features/nastyazz/impel/src/main/java/com/nastyazz/impel/nastyazz/domain/SearchItemUseCase.kt
+++ features/nastyazz/impel/src/main/java/com/nastyazz/impel/nastyazz/domain/SearchItemUseCase.kt
@@ -1,11 +1,29 @@
 package com.nastyazz.impel.nastyazz.domain
-import com.nastyazz.api.domain.usecases.ItemSuggestUseCase
+import com.example.nastyazz.api.NastyItem
+import com.nastyazz.api.domain.usecases.ItemSearchUseCase
 import com.nastyazz.impel.nastyazz.data.ItemRepository
+import com.nastyazz.api.domain.models.Item
 
-internal class ItemSuggestUseCaseImpl(
+internal class ItemSearchUseCaseImpl(
     private val repository: ItemRepository
-) : ItemSuggestUseCase {
-    override suspend fun invoke(): String? {
-        return runCatching { repository.savedSuggest() }.getOrNull()
+) : ItemSearchUseCase {
+
+    override suspend fun invoke(query: String): List<Item> {
+        if (query.isBlank()) return emptyList()
+
+        val items = repository.search(query).map {
+            Item(
+                id = it.id,
+                title = it.title,
+                description = it.description,
+                imageUrl = it.thumbnail
+            )
+        }
+
+        if (items.isNotEmpty()) {
+            repository.saveSuggest(query)
+        }
+
+        return items
     }
 }
