22425eee9e7d072b33d067497574994c17639b82
roganofff <matematic1337@gmail.com>
2025-12-24 11:32:18 +0300

    Clean code again

--- features/quovadis/impl/src/main/java/quo/vadis/impl/domain/GetCatNameUseCaseImpl.kt
+++ features/quovadis/impl/src/main/java/quo/vadis/impl/domain/GetCatNameUseCaseImpl.kt
@@ -4,7 +4,6 @@ import com.example.api.CatNameRepository
 import com.example.injector.inject
 import quo.vadis.api.model.Cat
 import quo.vadis.api.usecases.GetCatNameUseCase
-import quo.vadis.impl.data.CatRepositoryImpl
 
 internal class GetCatNameUseCaseImpl : GetCatNameUseCase {
     private val repository: CatNameRepository by inject()
