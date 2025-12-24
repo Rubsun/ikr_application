b7ab45f11ede75553ce8b62ff2eac89552faf817
nastyazz <nastayz0808@mail.ru>
2025-12-24 16:49:28 +0300

    Revert "fix"
    This reverts commit 2f291a920cb4d4bb971471d8e5e9a880a82068e9.

--- features/nastyazz/impel/src/main/java/com/nastyazz/impel/nastyazz/domain/SearchItemUseCase.kt
+++ features/nastyazz/impel/src/main/java/com/nastyazz/impel/nastyazz/domain/SearchItemUseCase.kt
@@ -1,29 +1,11 @@
 package com.nastyazz.impel.nastyazz.domain
-import com.example.nastyazz.api.NastyItem
-import com.nastyazz.api.domain.usecases.ItemSearchUseCase
+import com.nastyazz.api.domain.usecases.ItemSuggestUseCase
 import com.nastyazz.impel.nastyazz.data.ItemRepository
-import com.nastyazz.api.domain.models.Item
 
-internal class ItemSearchUseCaseImpl(
+internal class ItemSuggestUseCaseImpl(
     private val repository: ItemRepository
-) : ItemSearchUseCase {
-
-    override suspend fun invoke(query: String): List<Item> {
-        if (query.isBlank()) return emptyList()
-
-        val items = repository.search(query).map {
-            Item(
-                id = it.id,
-                title = it.title,
-                description = it.description,
-                imageUrl = it.thumbnail
-            )
-        }
-
-        if (items.isNotEmpty()) {
-            repository.saveSuggest(query)
-        }
-
-        return items
+) : ItemSuggestUseCase {
+    override suspend fun invoke(): String? {
+        return runCatching { repository.savedSuggest() }.getOrNull()
     }
 }
