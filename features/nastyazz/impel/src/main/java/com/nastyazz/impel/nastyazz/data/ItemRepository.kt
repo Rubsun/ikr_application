a77a84e13820480ef4986b9cca01ad4ae2a151d7
nastyazz <nastayz0808@mail.ru>
2025-12-24 16:49:31 +0300

    Revert "separate retrafit module"
    This reverts commit 649ac5c4a87d0e7f69a7498dbd02051722c5e6c0.

--- features/nastyazz/impel/src/main/java/com/nastyazz/impel/nastyazz/data/ItemRepository.kt
+++ features/nastyazz/impel/src/main/java/com/nastyazz/impel/nastyazz/data/ItemRepository.kt
@@ -1,23 +1,20 @@
 package com.nastyazz.impel.nastyazz.data
 
-import com.example.nastyazz.api.NastyAzzClient
-import com.example.nastyazz.api.NastyItem
 import com.example.primitivestorage.api.PrimitiveStorage
 import kotlinx.coroutines.flow.first
 
 private const val STORAGE_NAME = "nastyazz_item_suggest.json"
 
 internal class ItemRepository(
-    private val client: NastyAzzClient,
+    private val api: NastyAzzApi,
     private val storageFactory: PrimitiveStorage.Factory
 ) {
-
     private val storage by lazy {
         storageFactory.create(STORAGE_NAME, ItemSuggestDto.serializer())
     }
 
-    suspend fun search(query: String): List<NastyItem> {
-        return client.searchProducts(query)
+    suspend fun search(query: String): List<ItemDto> {
+        return api.search(query).products
     }
 
     suspend fun savedSuggest(): String? {
--- features/nastyazz/impel/src/main/java/com/nastyazz/impel/nastyazz/data/models/ItemDto.kt
+++ features/nastyazz/impel/src/main/java/com/nastyazz/impel/nastyazz/data/models/ItemDto.kt
@@ -1,1 +1,8 @@
 package com.nastyazz.impel.nastyazz.data.models
+
+internal data class ItemDto(
+    val id: Int,
+    val title: String,
+    val description: String,
+    val imageUrl: String
+)
